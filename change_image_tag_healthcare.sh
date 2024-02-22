b2e3f65099b0966d5bc6c01c4f86c7d217242801
venkateshkumarselvam <151203801+venkateshkumarselvam@users.noreply.github.com>
2024-02-22 20:52:38 +0530

    Update change_image_tag_healthcare.sh

--- change_image_tag_healthcare.sh
+++ change_image_tag_healthcare.sh
@@ -3,4 +3,4 @@
 # Extract the tag name from the command-line argument
 NEW_TAG="$1"
 # Replace the image tag in the Kubernetes YAML file
-sed -i "s/image: venkt06\/healthapp-w1-app:*/image: venkt06\/healthapp-w1-app:${NEW_TAG}/" kubectl apply -f k8s_deployment_healthcare.yaml
+sed -i "s/image: venkt06\/healthapp-w1-app:.*/image: venkt06\/healthapp-w1-app:${NEW_TAG}/" kubectl apply -f k8s_deployment_healthcare.yaml
